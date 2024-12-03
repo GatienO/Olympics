52367dc8214dfcfe7591ed9f55772c38119cc692
GatienO <oceanegatienpro@gmail.com>
2024-12-03 10:31:31 +0100

    home page with graphics

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,13 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { NotFoundComponent } from './pages/not-found/not-found.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
 import { DetailComponent } from './pages/detail/detail.component';
 
 const routes: Routes = [
   {
     path: '',
-    component: DashboardComponent,
+    component: HomeComponent,
   },
   {
     path: 'detail/:country',
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { OlympicService } from 'src/app/core/services/olympic.service';
+import { BaseChartDirective } from 'ng2-charts';
+import { Chart } from 'chart.js/auto';
 
 @Component({
   selector: 'app-home',
@@ -8,11 +10,71 @@ import { OlympicService } from 'src/app/core/services/olympic.service';
   styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent implements OnInit {
+  chart: any;
   public olympics$: Observable<any> = of(null);
 
   constructor(private olympicService: OlympicService) {}
 
   ngOnInit(): void {
+
     this.olympics$ = this.olympicService.getOlympics();
+    console.log(this.olympics$);
+
+
+    this.olympicService.getOlympics().subscribe(data => {
+      const countries = data.map((item: any) => item.country); // Noms des pays
+      const participations = data.map((item: any) => 
+        item.participations.reduce((sum: number, p: any) => sum + p.medalsCount, 0)
+      );
+      this.createChart(countries, participations);
+    });
   }
+
+
+  createChart(countries: string[], participations: number[]) {
+    const ctx = document.getElementById('pieChart') as HTMLCanvasElement;
+    this.chart = new Chart(ctx, {
+      type: 'pie',
+      data: {
+        labels: countries,
+        datasets: [
+          {
+            label: 'Total Medals',
+            data: participations,
+            backgroundColor: [
+              'rgba(255, 99, 132, 0.2)',
+              'rgba(54, 162, 235, 0.2)',
+              'rgba(255, 206, 86, 0.2)',
+              'rgba(75, 192, 192, 0.2)',
+              'rgba(153, 102, 255, 0.2)'
+            ],
+            borderColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)'
+            ],
+            borderWidth: 1,
+          },
+        ],
+      },
+      options: {
+        responsive: true,
+        plugins: {
+          legend: {
+            position: 'top',
+          },
+        },
+      },
+    });
+  }
+  
 }
+
+
+
+
+
+
+
