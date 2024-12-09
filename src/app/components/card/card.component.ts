ca2c9f32363fde46ca5ac7118d8ebe8b541a370a
GatienO <oceanegatienpro@gmail.com>
2024-12-09 13:01:01 +0100

    ts doc

--- src/app/components/card/card.component.ts
+++ src/app/components/card/card.component.ts
@@ -1,6 +1,14 @@
 import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+
+
+/**
+ * CardComponent
+ *
+ * This component is a reusable card that displays a list of items. Each item contains a title and a numeric value.
+ * It accepts data through an `@Input` property and displays the items dynamically in its template.
+ */
 @Component({
   selector: 'app-card',
   standalone: true,
@@ -10,8 +18,16 @@ import { CommonModule } from '@angular/common';
 })
 export class CardComponent {
   
+   /**
+   * List of items to be displayed in the card.
+   * Each item consists of a title and a value.
+   */
   @Input() items: { title: string; value: number }[] = [];
 
+  /**
+   * Lifecycle hook that is called after the component is initialized.
+   * Logs the received items to the console for debugging purposes.
+   */
   ngOnInit() {
     console.log('Received items:', this.items);
   }
